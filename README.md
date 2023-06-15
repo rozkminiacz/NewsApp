e5f4068f8fab68a122443c4dcb5741bdd684d848
jaroslawmichalik <michalik@protonmail.ch>
2023-06-15 11:16:25 +0200

    Update with kotest

---
+++ README.md
@@ -0,0 +1,8 @@
+# Kotlin Testing Masterclass
+
+- add secrets.gradle (copy-paste content from secrets.gradle.example)
+- get API key from [News API](https://newsapi.org/)
+
+- run example tests: `TestFrameworkIsWorking.kt` and `SomeSpec`
+
+- install [Kotest plugin](https://plugins.jetbrains.com/plugin/14080-kotest)
---
+++ app/src/test/java/com/kotlintesting/news/SomeSpec.kt
@@ -0,0 +1,20 @@
+package com.kotlintesting.news
+
+import io.kotest.core.spec.style.StringSpec
+
+/**
+ * Created by jaroslawmichalik on 15/06/2023
+ */
+class SomeSpec: StringSpec({
+  "it should pass"{
+
+  }
+
+  "it should fail"{
+    throw AssertionError(":(")
+  }
+
+  "it should crash"{
+    throw Exception(":(((")
+  }
+})
